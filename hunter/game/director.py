e011a9c486b0b69c580fc9e550d7faac1704dada
Jerry Marollano <70122455+chrmux@users.noreply.github.com>
2021-09-29 15:26:10 +0800

    Update director.py

--- hunter/game/director.py
+++ hunter/game/director.py
@@ -1,1 +1,16 @@
+import random
 
+class Director:
+  
+  def start_game():
+    pass
+  
+  def get_word_masked():
+    pass
+  
+  def get_guesses_left():
+    pass
+  
+  def get_word():
+    words=[]
+    return random.choice(words)
